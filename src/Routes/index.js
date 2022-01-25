56b30541395c0015ede9f3597a4511f2e8258e1e
dwisabrina23 <dwisabrina10@gmail.com>
2022-01-26 02:59:22 +0800

    idx

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import {Routes, Route, BrowserRouter} from "react-router-dom"
-import Navbar from '../Components/Navbar'
-import LandingPage from "../Pages/LandingPage/LandingPage";
-import MyReservation from '../Pages/MyReservation/MyReservation'
-import Reservation from '../Pages/Reservation/Reservation'
-import UserProfilePage from '../Pages/UserProfilePage/UserProfilePage'
-import Login from '../Pages/LoginPage/Login'
-import PersonalData from '../Pages/PersonalDataPage/PersonalData'
-
-function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<LandingPage />}></Route>
-                <Route path="/profile" element={<><UserProfilePage/></>}></Route>
-                <Route path="/reservasi-vaksin" element={<><Reservation/></>}></Route>
-                <Route path="/my-reservation" element={<><MyReservation/></>}></Route>
-                <Route path="/login" element={<><Navbar/><Login/></>}></Route>
-                <Route path="/profile" element={<><Navbar/><PersonalData/></>}></Route>
-            </Routes>
-        </BrowserRouter>
-    )
-=======
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LandingPage from "../Pages/LandingPage/LandingPage";
@@ -122,6 +97,5 @@ function Router() {
       </Routes>
     </BrowserRouter>
   );
->>>>>>> 47bacbed95282b5397778f0820389319e61a69c7
 }
 export default Router;
